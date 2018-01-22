60d91f6127b8f10d06222591c0e46fe1cc311d0b
Lennart Potthoff <lp782516@fh-muenster.de>
2018-01-22 16:20:39 +0100

    Rename

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from controller.py import *
+from controller import *
 
 while True:
-  if 1 == I1.get_state():
+  if True == I1.get_state():
     # Convert from degree (0..360) to internal (0..72); 72/360=1/5
-    M1.drive(5 / 5 , None)
+    M1.set_distance(5 / 5 , None)
   else:
     M1.stop()
